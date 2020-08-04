59db351f24958fd7220d9b222978db34d8fbda0e
spag007 <gabrielmeloche1@gmail.com>
2020-08-04 13:06:30 -0400

    Started implementing basic car

---
+++ Assets/Scripts/Vehicle/Wheel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    private WheelCollider m_wheel;
+
+    void Start()
+    {
+        m_wheel = GetComponentInParent<WheelCollider>();
+    }
+
+    void Update()
+    {
+        RotateWheelVisually();
+
+        Vector3 position;
+        Quaternion rotation;
+        m_wheel.GetWorldPose(out position, out rotation);
+
+        Debug.Log(gameObject.name + ": " + (rotation == transform.rotation));
+    }
+
+    public void RotateWheelVisually()
+    {
+        Vector3 position;
+        Quaternion rotation;
+        m_wheel.GetWorldPose(out position, out rotation);
+        //transform.position = position;
+        transform.rotation = rotation;
+    }
+}
